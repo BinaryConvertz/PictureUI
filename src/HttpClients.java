09c0c4019942ad04f398fa414fbd1722ce16d2fc
RYAN JAMES <realcoder2023@gmail.com>
2026-01-05 17:51:29 -0500

    Added: Link Passed By URL TO UI To Show Image on Screen

--- src/HttpClients.java
+++ src/HttpClients.java
@@ -1,18 +1,19 @@
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 
 public class HttpClients {
 
-    public static void GetServerClient(URL url) throws URISyntaxException {
+    public static URI GetServerClient(URL url) throws URISyntaxException {
 
-
-        HttpClient.newHttpClient();
-        HttpRequest.newBuilder().uri(
+       HttpRequest req = HttpRequest.newBuilder().uri(
                 URI.create(String.valueOf(url.toURI()))
         ).build();
 
+
+
+        return req.uri();
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,20 @@ void Close(JFrame frame) {
 
 void main() throws MalformedURLException, URISyntaxException {
 
-    UtilsClass m =new UtilsClass();
-    m.ImageSrc(new URI("https://avatars.githubusercontent.com/u/198405425?v=4"));
+   URI urlLink = HttpClients.GetServerClient(URI.create
+            ("https://avatars.githubusercontent.com/u/198405425?v=4").toURL());
     ArrayList<Integer> positions = new ArrayList<>();
     positions.add(90 * 5);
     positions.add(100 * 5);
 
+    ImageIcon icon = new ImageIcon(urlLink.toURL());
     JFrame frame = new JFrame("Valued App");
     for(int _ : positions) {
         frame.setSize(new Dimension(positions.getFirst(), positions.getLast()));
+        frame.add(new JLabel(icon));
+        frame.pack();
     }
+
 
     Close(frame);
     frame.setVisible(true);
